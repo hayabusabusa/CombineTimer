79d8d4fe31da0db0c268b5cb863970684f7408f3
hayabusabusa <hayabusabusaSY@gmail.com>
2020-08-19 13:39:22 +0900

    Add xcode project

---
+++ CombineTimer/Common/Button.swift
@@ -0,0 +1,67 @@
+//
+//  Button.swift
+//  CombineTimer
+//
+//  Created by 山田隼也 on 2020/08/06.
+//  Copyright © 2020 Shunya Yamada. All rights reserved.
+//
+
+import UIKit
+
+class Button: UIButton {
+
+    // MARK: IBInspectable
+    
+    @IBInspectable var cornerRadius: CGFloat = 0
+    @IBInspectable var borderColor: UIColor = .clear
+    @IBInspectable var borderWidth: CGFloat = 0
+    @IBInspectable var shadowRadius: CGFloat = 0
+    @IBInspectable var shadowColor: UIColor = .clear
+    @IBInspectable var shadowOffset: CGSize = .zero
+    @IBInspectable var shadowOpacity: Float = 0
+    
+    // MARK: Overrides
+    
+    override var isEnabled: Bool {
+        didSet {
+            alpha = isEnabled ? 1 : 0.4
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        commonInit()
+    }
+
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        commonInit()
+
+        if let title = title(for: state) {
+            setAttributedTitle(NSAttributedString(string: title,
+                                                  attributes: [NSAttributedString.Key(String(kCTLanguageAttributeName)): "ja"]),
+                               for: .normal)
+        }
+    }
+    
+    private func commonInit() {
+        isExclusiveTouch = true
+        layer.masksToBounds = true
+        layer.cornerRadius = cornerRadius
+        layer.borderColor = borderColor.cgColor
+        layer.borderWidth = borderWidth
+        layer.shadowColor = shadowColor.cgColor
+        layer.shadowOpacity = shadowOpacity
+        layer.shadowOffset = shadowOffset
+        layer.shadowRadius = shadowRadius
+    }
+}
---
+++ CombineTimer/View/TimerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TimerViewController.swift
+//  CombineTimer
+//
+//  Created by 山田隼也 on 2020/08/06.
+//  Copyright © 2020 Shunya Yamada. All rights reserved.
+//
+
+import UIKit
+
+final class TimerViewController: UIViewController {
+    
+    // MARK: IBOutlet
+    
+    // MARK: Properties
+    
+    // MARK: Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureNavigation()
+    }
+}
+
+extension TimerViewController {
+    
+    private func configureNavigation() {
+        navigationItem.title = "タイマー"
+    }
+}
