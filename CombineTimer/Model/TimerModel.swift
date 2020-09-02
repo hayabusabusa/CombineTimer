158d71e76ecef3ac87c3d06d2e138da003ea08d6
hayabusabusa <hayabusabusaSY@gmail.com>
2020-09-02 11:16:45 +0900

    Timer Model のインターバルを外部から指定可能に修正

--- CombineTimer/Model/TimerModel.swift
+++ CombineTimer/Model/TimerModel.swift
@@ -31,7 +31,7 @@ final class TimerModel: TimerModelProtocol {
 
     // MARK: Initializer
 
-    init() {
+    init(interval: TimeInterval = 1.0) {
         // NOTE: プロパティの初期化.
         let countSubject = CurrentValueSubject<Int, Never>(0)
         self.countSubject = countSubject
@@ -43,7 +43,7 @@ final class TimerModel: TimerModelProtocol {
         // NOTE: `FlatMapLatest` で最新の Publisher に切り替え.
         isValidSubject
             .flatMapLatest { isValid -> AnyPublisher<Date, Never> in
-                isValid ? Timer.publish(every: 1.0, on: .main, in: .default).autoconnect().eraseToAnyPublisher() : Empty<Date, Never>(completeImmediately: true).eraseToAnyPublisher()
+                isValid ? Timer.publish(every: interval, on: .main, in: .default).autoconnect().eraseToAnyPublisher() : Empty<Date, Never>(completeImmediately: true).eraseToAnyPublisher()
             }.sink { _ in
                 countSubject.send(countSubject.value + 1)
             }.store(in: &cancelables)
